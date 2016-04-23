0df25289399c109f090e8abf75e4a8ac91d405ce
Ted Shaffer <ted@brightsign.biz>
2016-04-23 15:53:58 -0700

    baseline

---
+++ javascripts/App.js
@@ -0,0 +1,16 @@
+/**
+ * Created by tedshaffer on 4/23/16.
+ */
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className = "container">
+                Eat Pizza!!
+            </div>
+        );
+    }
+}
+
+export default App;
